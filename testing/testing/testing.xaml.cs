13f3c28ebf5faa1b2e2d7f4825c130401d5e0fe9
inupumper <admin@Mac-Admin.local>
2016-09-12 08:35:08 +0300

    first update

---
+++ testing/testing/testing.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace testing
+{
+	public partial class testing : ContentPage
+	{
+		
+		public testing ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
+
